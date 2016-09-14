f6365b71807f2c93414e3bead5b4331ff91cce1a
Danil Skachkov <danil.skachkov@speaktoit.com>
2016-09-14 17:21:49 +0700

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Module must be started with environment variables
 //
 //  accesskey="api.ai client access key"
-//  subscriptionkey="api.ai subscription key"
 //  slackkey="slack bot key"
 //
 
@@ -11,7 +10,6 @@ const Botkit = require('botkit');
 
 const apiai = require('apiai');
 const uuid = require('node-uuid');
-
 const http = require('http');
 
 const Entities = require('html-entities').XmlEntities;
@@ -22,7 +20,7 @@ const slackBotKey = process.env.slackkey;
 
 const apiAiService = apiai(apiAiAccessToken);
 
-var sessionIds = {};
+const sessionIds = new Map();
 
 const controller = Botkit.slackbot({
     debug: false
@@ -40,7 +38,7 @@ controller.on('rtm_close', function (bot, err) {
 
         // sometimes connection closing, so, we should restart bot
         if (bot.doNotRestart != true) {
-            var token = bot.config.token;
+            let token = bot.config.token;
             console.log('Trying to restart bot ' + token);
 
             restartBot(bot);
@@ -57,7 +55,7 @@ function restartBot(bot) {
             console.error('Error restarting bot to Slack:', err);
         }
         else {
-            var token = bot.config.token;
+            let token = bot.config.token;
             console.log('Restarted bot for %s', token);
         }
     });
@@ -75,8 +73,7 @@ function isDefined(obj) {
     return obj != null;
 }
 
-controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
-
+controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambient'], (bot, message) => {
     try {
         if (message.type == 'message') {
             if (message.user == bot.identity.id) {
@@ -87,12 +84,13 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
             }
             else {
 
-                var requestText = decoder.decode(message.text);
+                let requestText = decoder.decode(message.text);
                 requestText = requestText.replace("’", "'");
 
-                var channel = message.channel;
-                var messageType = message.event;
-                var botId = "<@" + bot.identity.id + ">";
+                let channel = message.channel;
+                let messageType = message.event;
+                let botId = '<@' + bot.identity.id + '>';
+                let userId = message.user;
 
                 console.log(requestText);
                 console.log(messageType);
@@ -101,51 +99,69 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
                     requestText = requestText.replace(botId, '');
                 }
 
-                if (!(channel in sessionIds)) {
-                    sessionIds[channel] = uuid.v1();
+                if (!sessionIds.has(channel)) {
+                    sessionIds.set(channel, uuid.v1());
                 }
 
-                var request = apiAiService.textRequest(requestText,
+                console.log('Start request ', requestText);
+                let request = apiAiService.textRequest(requestText,
                     {
-                        sessionId: sessionIds[channel]
+                        sessionId: sessionIds.get(channel),
+                        contexts: [
+                            {
+                                name: "generic",
+                                parameters: {
+                                    slack_user_id: userId,
+                                    slack_channel: channel
+                                }
+                            }
+                        ]
                     });
 
-                request.on('response', function (response) {
+                request.on('response', (response) => {
                     console.log(response);
 
                     if (isDefined(response.result)) {
-                        var responseText = response.result.fulfillment.speech;
-                        var action = response.result.action;
+                        let responseText = response.result.fulfillment.speech;
+                        let responseData = response.result.fulfillment.data;
 
-                        if (isDefined(responseText)) {
-                            bot.reply(message, responseText, function (err, resp) {
-                                console.log(err, resp);
-                            });
+                        if (isDefined(responseData) && isDefined(responseData.slack)) {
+                            replyWithData(bot, message, responseData);
+                        } else if (isDefined(responseText)) {
+                            replyWithText(bot, message, responseText);
                         }
 
                     }
                 });
 
-                request.on('error', function (error) {
-                    console.error(error);
-                });
-
+                request.on('error', (error) => console.error(error));
                 request.end();
             }
         }
     } catch (err) {
         console.error(err);
     }
-
 });
+
+function replyWithText(bot, message, responseText) {
+    bot.reply(message, responseText, (err, resp) => {
+        if (err) {
+            console.error(err);
+        }
+});
+}
+
+function replyWithData(bot, message, responseData) {
+    try {
+        bot.reply(message, responseData.slack);
+    } catch (err) {
+        bot.reply(message, err.message);
+    }
+}
 
 
 //Create a server to prevent Heroku kills the bot
-var server = http.createServer(function (req, res) {
-    res.end();
-});
+const server = http.createServer((req, res) => res.end());
 
 //Lets start our server
-server.listen((process.env.PORT || 5000), function () {
-    console.log("Server listening");
-});
+server.listen((process.env.PORT || 5000), () => console.log("Server listening"));
